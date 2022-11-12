be0b80d8b905b52f1b2ec85a62ef649ba406e303
Tenacom developer <info@tenacom.it>
2022-11-12 23:20:02 +0100

    Replace PolyKit.Embedded with PolyKit and unify public APIs across TFMs

--- src/AaaLib/Aaa.cs
+++ src/AaaLib/Aaa.cs
@@ -1,9 +1,7 @@
 ﻿// Copyright (C) Tenacom and Contributors. Licensed under the MIT license.
 // See the LICENSE file in the project root for full license information.
 
-#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_0_OR_GREATER
 using System;
-#endif
 
 namespace AaaLib;
 
@@ -23,19 +21,10 @@ public static class Aaa
     /// </remarks>
     public static int GetTheAnswer() => 42;
 
-#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_0_OR_GREATER
     /// <summary>
     /// Gets the answer to the ultimate question of life, the universe, and everything,
     /// as a read-only span of characters, in Italian.
     /// </summary>
     /// <returns>La risposta ("the answer", in Italian).</returns>
     public static ReadOnlySpan<char> GetTheAnswerInItalian() => Quarantadue.AsSpan();
-#else
-    /// <summary>
-    /// Gets the answer to the ultimate question of life, the universe, and everything,
-    /// as a string, in Italian.
-    /// </summary>
-    /// <returns>La risposta ("the answer", in Italian).</returns>
-    public static string GetTheAnswerInItalian() => Quarantadue;
-#endif
 }
