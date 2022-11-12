fd9dacc1159e487b06c30cb831c6d25477df6ba9
Tenacom developer <info@tenacom.it>
2022-11-12 17:29:43 +0100

    Add a test project

---
+++ tests/AaaLibTests/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿// Copyright (C) Tenacom and Contributors. Licensed under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+global using AaaLib;
+global using FluentAssertions;
+global using Xunit;
